e317b31045d1f1f98d4c560577d08c015033a12e
Adil Sebbary <akadilou@gmail.com>
2026-04-25 20:41:12 +0100

    fix: start.sh alembic versions list

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, text
 engine = create_engine(os.environ['DATABASE_URL'])
 with engine.connect() as conn:
     conn.execute(text('CREATE TABLE IF NOT EXISTS alembic_version (version_num VARCHAR(32) NOT NULL PRIMARY KEY)'))
-    for v in ['001', '002']:
+    for v in ['001', '002', '003', '004', '005']:
         existing = conn.execute(text(f\"SELECT version_num FROM alembic_version WHERE version_num='{v}'\")).fetchone()
         if not existing:
             conn.execute(text(f\"INSERT INTO alembic_version VALUES ('{v}')\"))
